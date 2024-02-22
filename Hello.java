e46e9002726ceefd16e620bd26a04184549df763
Amit Dhakal <amit.dhakal423@gmail.com>
2024-02-22 11:32:05 +0545

    final commit

--- Hello.java
+++ Hello.java
@@ -1,9 +1,16 @@
 public class Hello{
+public String home(){
 
+return "Hello World ";
+
+}
 
 public static void main(String[] args){
 
 System.out.println("Hello JENKIN THIS IS ME AMIT");
+System.out.println("hello i ama the jenkins ");
+Sytem.out.println("Hello Nepal From AMIT");
+
 
 }
 
